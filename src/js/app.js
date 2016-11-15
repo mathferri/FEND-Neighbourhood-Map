3cd31b44070b38887c5a4b784e690092b01c77bc
Matheus Henrique Ferri <Matheus Henrique Ferri>
2016-11-15 09:58:55 -0200

    Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/* MAP */
+
+var map;
+
+function initMap() {
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -25.4392395, lng: -49.2886484},
+        zoom: 16,
+        mapTypeControl: false
+    });
+}
